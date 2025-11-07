934c166188d8a6599b162f5d40406833c168f01f
AndreNobree <afelipe1405@gmail.com>
2025-11-07 18:35:23 -0300

    criação tela home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,19 @@
 import Logo from '../../assets/AlCommitLogo.png';
+import Config from '../../assets/configs.png';
+
 import './Home.css';
 
 function Home() {
   return (
     <div className="App">
       <div className='top'>
-        <h1 className='tittle-aplication'>AlCommit</h1>
-        <img src={Logo} alt="AlCommit Logo" className='logo-top'/>
+        <div className='left-top'>
+          <h1 className='tittle-aplication'>AlCommit</h1>
+          <img src={Logo} alt="AlCommit Logo" className='logo-top'/>
+        </div>
+        <div className='right-top'>
+          <img src={Config} alt="Configuration Icon" className='setting-top'/>
+        </div>
       </div>
     </div>
   );
